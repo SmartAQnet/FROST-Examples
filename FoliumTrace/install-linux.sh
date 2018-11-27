dd2bc49e87be31400f104f684f1f65c81a8d1deb
Marcel Köpke <marcel.koepke@gmail.com>
2018-11-27 11:23:41 +0100

    working linux install

--- FoliumTrace/install-linux.sh
+++ FoliumTrace/install-linux.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
 rm -f Pipfile.lock
-sudo apt-get update
-sudo apt-get install build-essential python-all-dev
 
 python -m pip install pip --upgrade
 pip install pipenv --upgrade
 
 pipenv install
-
-wget http://download.osgeo.org/gdal/2.3.2/gdal-2.3.2.tar.gz
-tar xvfz gdal-2.3.2.tar.gz
-cd gdal-2.3.2
-./configure --with-python
-make
-sudo make install
-cd ..
 
 pipenv run pip install shapely==1.6.4
 pipenv run pip install fiona==1.8.2
